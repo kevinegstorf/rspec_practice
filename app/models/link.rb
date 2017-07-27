2ce1c14386b9bd66ea66f388289ebea2ad1a1f52
Kevin <kevin.egstorf@gmail.com>
2017-07-27 14:14:15 +0200

    added up and downvote feature

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,8 +1,30 @@
 class Link < ActiveRecord::Base
+  IMAGE_FORMATS = %w(.jpg .gif .png)
+
   validates :title, presence: true
   validates :url, presence: true
 
+  def self.hottest_first
+    order("upvotes - downvotes DESC")
+  end
+
+  def self.newest_first
+    order(created_at: :desc)
+  end
+
   def upvote
     increment!(:upvotes)
+  end
+
+  def downvote
+    increment!(:downvotes)
+  end
+
+  def score
+    Score.new(self)
+  end
+
+  def image?
+    url.end_with? *IMAGE_FORMATS
   end
 end
---
+++ spec/support/api_helpers.rb
@@ -0,0 +1,10 @@
+# spec/support/api_helpers.rb
+module ApiHelpers
+  def json_body
+    JSON.parse(response.body)
+  end
+end
+
+RSpec.configure do |config|
+  config.include ApiHelpers, type: :request
+end
